afe5561134b60c6a7787c14072001f2fd2b5436d
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-01-01 20:40:43 +0100

    new changes made

---
+++ script.js
@@ -0,0 +1,84 @@
+let bill = 0;
+
+const products = [
+    {
+        name: "Absolut",
+        price: 19
+    },
+    {
+        name: "Smirnoff",
+        price: 13
+    },
+    {
+        name: "Skyy",
+        price: 13
+    },
+    {
+        name: "Ciroc",
+        price: 27
+    },
+    {
+        name: "Jameson",
+        price: 22
+    },
+    {
+        name: "Jack Daniel's",
+        price: 22
+    },
+    {
+        name: "Johnny Walker",
+        price: 23
+    },
+    {
+        name: "Malibu",
+        price: 20
+    },
+    {
+        name: "Bacardi",
+        price: 15
+    },
+    {
+        name: "Captain Morgan",
+        price: 18
+    },
+    {
+        name: "Havana Club",
+        price: 22
+    },
+    {
+        name: "Remy Martin",
+        price: 37
+    },
+    {
+        name: "Hennessy VSOP",
+        price: 99
+    },
+    {
+        name: "Martell VSOP",
+        price: 52
+    },
+    {
+        name: "Bombay",
+        price: 22
+    },
+    {
+        name: "Seagram's",
+        price: 12
+    },
+    {
+        name: "Gordon's",
+        price: 13
+    },
+    {
+        name: "Casamigos",
+        price: 50
+    },
+    {
+        name: "1800 Silver",
+        price: 26
+    },
+    {
+        name: "Don Julio 1942",
+        price: 105
+    }
+];
